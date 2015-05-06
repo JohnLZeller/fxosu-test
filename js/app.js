10f14826aa48163d75970315f8cf2857ddfeedbf
John Zeller <johnlzeller@gmail.com>
2015-05-05 19:23:34 -0700

    Waves are now random, tracking data use, interrupts start from same place

--- js/app.js
+++ js/app.js
@@ -29,22 +29,51 @@ window.addEventListener('DOMContentLoaded', function() {
     // FXOS: Mozilla/5.0 (Mobile; rv:40.0) Gecko/40.0 Firefox/40.0
   var requestWith = null;
   var requestWithout = null;
+  var timeoutWith = null;
+  var timeoutWithout = null;
+  var firstInterrupt = null;
+  var n = 5;
+  var wave = null;
+
   var attemptsWith = 0;
   var attemptsWithout = 0;
   var successesWith = 0;
   var successesWithout = 0;
+  var dataWith = 0;
+  var dataWithout = 0;
+  var batUsageWith = {base: 0, diff: 0};
+  var batUsageWithout = {base: 0, diff: 0};
+  var memUsageWith = {base: 0, diff: 0};
+  var memUsageWithout = {base: 0, diff: 0};
+  var latUsageWith = {base: 0, diff: 0};
+  var latUsageWithout = {base: 0, diff: 0};
+  var startTime = 0;
+
+  var nTimeout = 1000;
   var level = 2;
   var r = null;
-  var nWith = 30;
-  var nWithout = 30;
+
   var translate = navigator.mozL10n.get;
+
+  // Table Elements
   var benchmarkLevel = document.getElementById('benchmarkLevel');
   var progressBar = document.getElementById('progress');
+  var netStatus = document.getElementById('netStatus');
+  var intStatus = document.getElementById('intStatus');
+  var rateWithout = document.getElementById('sucWithout');
+  var rateWith = document.getElementById('sucWith');
   var atmWithout = document.getElementById('attemptsWithout');
   var atmWith = document.getElementById('attemptsWith');
   var sucWithout = document.getElementById('successesWithout');
   var sucWith = document.getElementById('successesWith');
-  var netStatus = document.getElementById('netStatus');
+  var batWithout = document.getElementById('batWithout');
+  var batWith = document.getElementById('batWith');
+  var memWithout = document.getElementById('memWithout');
+  var memWith = document.getElementById('memWith');
+  var dWithout = document.getElementById('dataWithout');
+  var dWith = document.getElementById('dataWith');
+  var latWithout = document.getElementById('latWithout');
+  var latWith = document.getElementById('latWith');
 
   var fxosutest = document.getElementById('fxosutest');
   fxosutest.addEventListener('click', function(e) {
@@ -55,6 +84,7 @@ window.addEventListener('DOMContentLoaded', function() {
   runbenchmark.addEventListener('click', function(e) {
     runBenchmark();
   }, false);
+
 
   function fxosuTest() {
 
@@ -121,11 +151,19 @@ window.addEventListener('DOMContentLoaded', function() {
 
   }
 
-  // Currently runs in waves of 5 secs on, 5 secs off
-  function getInterrupt() {
-    var num = (new Date()).getTime() % 10000;
 
-    if (num > 5000) {
+  function getInterrupt() {
+    var ms = wave * 2;
+
+    // Set starting times
+    if (atmWithout === 1 || atmWith === 1) {
+      startTime = (new Date()).getTime();
+    }
+
+    // Determine number based on startTime, to ensure we start at 0
+    var num = ((new Date()).getTime() - startTime) % ms;
+
+    if (num > wave) {
       netStatus.innerHTML = "Unstable";
       return true;
     } else {
@@ -136,16 +174,10 @@ window.addEventListener('DOMContentLoaded', function() {
 
 
   function runBenchmark() {
-    //_sendFail();
-    //return;
-
-    // Update table
-    atmWithout.innerHTML = 0;
-    atmWith.innerHTML = 0;
-    sucWithout.innerHTML = 0;
-    sucWith.innerHTML = 0;
-    netStatus.innerHTML = "Unknown";
-    progressBar.setAttributeNS(null, 'width', '0%');
+    wave = parseInt(Math.random() * 5000); // Makes a random network change of 0 to 5 seconds
+    resetLabels();
+    resetCounters();
+    intStatus.innerHTML = wave + " ms waves";
 
     switch (benchmarkLevel.value) {
       case "1 - High":
@@ -162,16 +194,11 @@ window.addEventListener('DOMContentLoaded', function() {
         break;
     }
 
-    // Reset our counters
-    attemptsWith = 0;
-    attemptsWithout = 0;
-    successesWith = 0;
-    successesWithout = 0;
-
+    // Just call without, once it finishes its' 30 requests, onRequestLoad will handle starting sendRequestWith()
     sendRequestWithout();
-    sendRequestWith();
 
   }
+
 
   function _sendFail () {
     // mozSystem option required to prevent CORS errors (Cross Origin Resource Sharing)
@@ -184,10 +211,10 @@ window.addEventListener('DOMContentLoaded', function() {
 
     r.open('get', urlFail + '?_=' + (new Date()).getTime(), true); // Appending a rand to tail of url, ensures no caching
     r.send();
-    console.log("Did the thing!");
+    //console.log("Did the thing!");
   }
 
-  // TODO: Implement with Web Workers? Multi-threaded :)
+
   function sendRequestWithout() {
     attemptsWithout += 1;
 
@@ -207,18 +234,18 @@ window.addEventListener('DOMContentLoaded', function() {
     if (!getInterrupt()) { // success
       requestWithout.open('get', urlSuccess + '?_=' + (new Date()).getTime(), true); // Appending a rand to tail of url, ensures no caching
       requestWithout.send();
-      console.log("Without: Sent SUCCESS request (#" + attemptsWithout + ")");
+      //console.log("Without: Sent SUCCESS request (#" + attemptsWithout + ")");
     } else { // fail
       requestWithout.open('get', urlFail + '?_=' + (new Date()).getTime(), true); // Appending a rand to tail of url, ensures no caching
       requestWithout.send();
-      console.log("Without: Sent FAIL request (#" + attemptsWithout + ")");
+      //console.log("Without: Sent FAIL request (#" + attemptsWithout + ")");
     }
 
-    //console.log("Without: Request #" + attemptsWithout + " was received! Woohoo! On to the next one!");
+    // Set a timeout to keep from freezing
+    timeoutWithout = setTimeout(function(){ timeoutCalled(sendRequestWithout); }, nTimeout);
   }
 
 
-  // TODO: Implement with Web Workers? Multi-threaded :)
   function sendRequestWith() {
     while (1) {
       if (navigator.mozFxOSUService.mozIsNowGood(level)) {
@@ -240,11 +267,11 @@ window.addEventListener('DOMContentLoaded', function() {
         if (!getInterrupt()) { // success
           requestWith.open('get', urlSuccess + '?_=' + (new Date()).getTime(), true); // Appending a rand to tail of url, ensures no caching
           requestWith.send();
-          console.log("With: Sent SUCCESS request (#" + attemptsWith + ")");
+          //console.log("With: Sent SUCCESS request (#" + attemptsWith + ")");
         } else { // fail
           requestWith.open('get', urlFail + '?_=' + (new Date()).getTime(), true); // Appending a rand to tail of url, ensures no caching
           requestWith.send();
-          console.log("With: Sent FAIL request (#" + attemptsWith + ")");
+          //console.log("With: Sent FAIL request (#" + attemptsWith + ")");
         }
 
         // You did your thang, now leave
@@ -252,7 +279,8 @@ window.addEventListener('DOMContentLoaded', function() {
       }
     }
 
-    //console.log("With: Request #" + attemptsWith + " was received! Woohoo! On to the next one!");
+    // Set a timeout to keep from freezing
+    timeoutWith = setTimeout(function(){ timeoutCalled(sendRequestWith); }, nTimeout);
   }
 
 
@@ -275,59 +303,177 @@ window.addEventListener('DOMContentLoaded', function() {
     // Update table
     atmWithout.innerHTML = attemptsWithout;
     atmWith.innerHTML = attemptsWith;
-    var pro = ((successesWithout + successesWith) / (nWithout + nWith)) * 100;
+    var pro = ((successesWithout + successesWith) / (n * 2)) * 100;
     progressBar.setAttributeNS(null, 'width', pro.toString() + '%');
 
     if (func === sendRequestWithout) {
       contentLen = requestWithout.getResponseHeader("Content-Length");
       statusCode = requestWithout.status;
+      clearTimeout(timeoutWithout);
+      dataWithout += parseInt(contentLen);
 
       // Check status of reply
       if (statusCode !== 200) { // If fail, retry
-        console.log("Without: Response #" + attemptsWithout + " FAILED with " + statusCode);
+        //console.log("Without: Response #" + attemptsWithout + " FAILED with " + statusCode);
         sendRequestWithout();
       } else if (statusCode === 0) {
-        console.log("Without: Response #" + attemptsWithout + " ERROR of come kind");
+        //console.log("Without: Response #" + attemptsWithout + " ERROR of come kind");
+        sendRequestWithout();
       } else { // If success, let next request happen
         successesWithout += 1;
         sucWithout.innerHTML = successesWithout;
-        console.log("Without: Response #" + attemptsWithout + " SUCCEEDED");
-        if (successesWithout < nWithout) {
+        //console.log("Without: Response #" + attemptsWithout + " SUCCEEDED");
+        if (successesWithout < n) {
           sendRequestWithout();
         } else {
-          // Set Data
-          document.getElementById('sucWithout').innerHTML = ((nWithout / attemptsWithout) * 100).toFixed(2) + "% (" + 
-                                                            nWithout + "/" + attemptsWithout + ")";
+          setWithoutData();
+
+          // sendRequestWithout() has finished its' 30 requests, now start sendRequestWith()
+          sendRequestWith();
         }
       }
     } else if (func === sendRequestWith) {
       contentLen = requestWith.getResponseHeader("Content-Length");
       statusCode = requestWith.status;
+      clearTimeout(timeoutWith);
+      dataWith += parseInt(contentLen);
 
       // Check status of reply
       if (statusCode !== 200) { // If fail, retry
-        console.log("With: Response #" + attemptsWith + " FAILED with " + statusCode);
+        //console.log("With: Response #" + attemptsWith + " FAILED with " + statusCode);
         sendRequestWith();
       } else if (statusCode === 0) {
-        console.log("With: Response #" + attemptsWith + " ERROR of come kind");
+        //console.log("With: Response #" + attemptsWith + " ERROR of come kind");
+        sendRequestWith();
       } else { // If success, let next request happen
         successesWith += 1;
         sucWith.innerHTML = successesWith;
-        console.log("With: Response #" + attemptsWith + " SUCCEEDED");
-        if (successesWith < nWith) {
+        //console.log("With: Response #" + attemptsWith + " SUCCEEDED");
+        if (successesWith < n) {
           sendRequestWith();
         } else {
-          document.getElementById('sucWith').innerHTML = ((nWith / attemptsWith) * 100).toFixed(2) + "% (" + 
-                                                            nWith + "/" + attemptsWith + ")";
+          setWithData();
         }
       }
     }
   }
 
 
+  function setWithoutData() {
+    rateWithout.innerHTML = ((n / attemptsWithout) * 100).toFixed(2) + "% (" + 
+                                                      n + "/" + attemptsWithout + ")";
+    dWithout.innerHTML = dataWithout;
+  }
+
+
+  function setWithData() {
+    rateWith.innerHTML = ((n / attemptsWith) * 100).toFixed(2) + "% (" + 
+                                                      n + "/" + attemptsWith + ")";
+    dWith.innerHTML = dataWith;
+    highlightWinners();
+  }
+
+
+  function highlightWinners() {
+    // Success Rate and Attempts
+    if (attemptsWithout > attemptsWith) {
+      rateWith.style.color = "green";
+      atmWith.style.color = "green";
+    } else if (attemptsWithout < attemptsWith) {
+      rateWithout.style.color = "green";
+      atmWithout.style.color = "green";
+    }
+
+    // Battery
+    if (batUsageWithout.diff > batUsageWith.diff) {
+      batWith.style.color = "green";
+    } else if (batUsageWithout.diff < batUsageWith.diff) {
+      batWithout.style.color = "green";
+    }
+
+    // Memory
+    if (memUsageWithout.diff > memUsageWith.diff) {
+      memWith.style.color = "green";
+    } else if (memUsageWithout.diff < memUsageWith.diff) {
+      memWithout.style.color = "green";
+    }
+
+    // Data Usage
+    if (dataWithout > dataWith) {
+      dWith.style.color = "green";
+    } else if (dataWithout < dataWith) {
+      dWithout.style.color = "green";
+    }
+
+    // Latency
+    if (latUsageWithout.diff > latUsageWith.diff) {
+      latWith.style.color = "green";
+    } else if (latUsageWithout.diff < latUsageWith.diff) {
+      latWithout.style.color = "green";
+    }
+  }
+
+  function resetLabels() {
+    progressBar.setAttributeNS(null, 'width', '0%');
+    netStatus.innerHTML = "Unknown";
+    intStatus.innerHTML = "";
+    rateWithout.innerHTML = "";
+    rateWith.innerHTML = "";
+    atmWithout.innerHTML = "";
+    atmWith.innerHTML = "";
+    sucWithout.innerHTML = "";
+    sucWith.innerHTML = "";
+    batWithout.innerHTML = "";
+    batWith.innerHTML = "";
+    memWithout.innerHTML = "";
+    memWith.innerHTML = "";
+    dWithout.innerHTML = "";
+    dWith.innerHTML = "";
+    latWithout.innerHTML = "";
+    latWith.innerHTML = "";
+  }
+
+
+  function resetCounters() {
+    firstInterrupt = null;
+    attemptsWith = 0;
+    attemptsWithout = 0;
+    successesWith = 0;
+    successesWithout = 0;
+    dataWith = 0;
+    dataWithout = 0;
+    batUsageWith.base = 0;
+    batUsageWith.diff = 0;
+    batUsageWithout.base = 0;
+    batUsageWithout.diff = 0;
+    memUsageWith.base = 0;
+    memUsageWith.diff = 0;
+    memUsageWithout.base = 0;
+    memUsageWithout.diff = 0;
+    latUsageWith.base = 0;
+    latUsageWith.diff = 0;
+    latUsageWithout.base = 0;
+    latUsageWithout.diff = 0;
+  }
+
+
+  function timeoutCalled(func) {
+    atmWithout.innerHTML = attemptsWithout;
+    atmWith.innerHTML = attemptsWith;
+
+    if (func === sendRequestWithout) {
+      clearTimeout(timeoutWithout);
+      sendRequestWithout();
+    } else if (func === sendRequestWith) {
+      clearTimeout(timeoutWith);
+      sendRequestWith();
+    }
+  }
+
+
   function showError(text) {
     // Failure COULD be because there is no CORS support on requested api
-    alert("ERROR: " + text);
+    console.log("ERROR: " + text);
   }
 
 });
